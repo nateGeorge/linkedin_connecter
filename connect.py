37b9e29ff7878b0f5e474f39f32ebd48c90d249b
wordsforthewise <wordsforthewise@gmail.com>
2017-02-26 11:10:50 -0700

    FEAT: debugging login page

--- connect.py
+++ connect.py
@@ -1,6 +1,7 @@
 # designed for Python 2
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 import os
 
@@ -23,9 +24,19 @@ if __name__ == "__main__":
         UNAME = raw_input('enter linkedin username:\n')
         PASS = raw_input('enter linkedin password:\n')
 
+    # first, login
     driver = setup_driver()
     driver.get(URL)
+    ## save page for inspection
+    with open('test.html', 'w') as f:
+         f.write(src.encode('ascii', 'ignore'))
+
     res = driver.page_source
     soup = bs(res)
-    button_class = 'mn-person-card__person-btn-ext'
-    soup.find_all({'class':button_class})
+    login_id = driver.find_element_by_id('session_key-login')
+    login_id.send_keys(UNAME)
+    login_pass = driver.find_element_by_id('session_password-login')
+    login_pass.send_keys(PASS)
+    login_pass.send_keys(Keys.ENTER) #submits form
+    # button_class = 'mn-person-card__person-btn-ext'
+    # soup.find_all({'class':button_class})
